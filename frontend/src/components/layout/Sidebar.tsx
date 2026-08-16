756185ae6115c3b75063b4539068236796b357db
Skiftstar <yukamitft@gmail.com>
2026-08-16 19:39:10 +0200

    feat(frontend): Add Material Icons to Sidebar

--- frontend/src/components/layout/Sidebar.tsx
+++ frontend/src/components/layout/Sidebar.tsx
@@ -1,14 +1,22 @@
+import MonitorHeartOutlinedIcon from "@mui/icons-material/MonitorHeartOutlined";
+import ViewInArOutlinedIcon from "@mui/icons-material/ViewInArOutlined";
+import FolderOpenOutlinedIcon from "@mui/icons-material/FolderOpenOutlined";
+import WifiOutlinedIcon from "@mui/icons-material/WifiOutlined";
+import AccessTimeOutlinedIcon from "@mui/icons-material/AccessTimeOutlined";
+import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined";
+import TerminalOutlinedIcon from "@mui/icons-material/TerminalOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import { NavLink } from "react-router";
 
 const navItems = [
-  { to: "/overview", label: "Overview" },
-  { to: "/container", label: "Containers" },
-  { to: "/services", label: "Services" },
-  { to: "/caddy", label: "Caddy" },
-  { to: "/cron", label: "Cron" },
-  { to: "/scripts", label: "Scripts" },
-  { to: "/terminal", label: "Terminal" },
-  { to: "/settings", label: "Settings" },
+  { to: "/overview", label: "Overview", icon: <MonitorHeartOutlinedIcon /> },
+  { to: "/containers", label: "Containers", icon: <ViewInArOutlinedIcon /> },
+  { to: "/services", label: "Services", icon: <FolderOpenOutlinedIcon /> },
+  { to: "/caddy", label: "Caddy", icon: <WifiOutlinedIcon /> },
+  { to: "/cron", label: "Cron", icon: <AccessTimeOutlinedIcon /> },
+  { to: "/scripts", label: "Scripts", icon: <DescriptionOutlinedIcon /> },
+  { to: "/terminal", label: "Terminal", icon: <TerminalOutlinedIcon /> },
+  { to: "/settings", label: "Settings", icon: <SettingsOutlinedIcon /> },
 ];
 
 export default function Sidebar() {
@@ -20,13 +28,14 @@ export default function Sidebar() {
             key={item.to}
             to={item.to}
             className={({ isActive }) =>
-              `block rounded-md px-3 py-2 text-sm font-medium transition-colors ${
+              `flex flex-row gap-2 items-center rounded-md px-3 py-2 text-sm font-medium transition-colors ${
                 isActive
                   ? "bg-accent/30 text-text/72"
                   : "text-text/72 hover:bg-accent-2/30"
               }`
             }
           >
+            {item.icon}
             {item.label}
           </NavLink>
         ))}
